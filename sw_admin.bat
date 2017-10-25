fd9d671c38f358dbe878728741d65f786a68b6bc
Clive Liu <liuxusheng@auto-link.com.cn>
2017-10-25 15:30:38 +0800

    Add sw_admin.bat sw_guest.bat

---
+++ sw_admin.bat
@@ -0,0 +1,3 @@
+@net use X: \\192.168.2.115\研发中心_软件部_软件版本发布 "Rduser2016" /user:"rduser"
+@echo Done to remote mount(sw_admin)
+@pause
---
+++ sw_guest.bat
@@ -0,0 +1,3 @@
+@net use Y: \\192.168.2.115\研发中心_软件部_软件版本发布 "rdread" /user:"rdread"
+@echo Done to remote mount(sw_guest)
+@pause
